cef6a1406fcac7f43f957168136a87055626ea86
norman-andrians <norman25.na@gmail.com>
2021-07-09 17:06:34 +0700

    update to 0.44, script logonya jadi useless njirr

---
+++ script/credits/logoImagesRotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class logoImagesRotation : MonoBehaviour
+{
+    public float rotateSpeed;
+
+    void Start()
+    {
+
+    }
+
+    public void isRotating(bool onRotating)
+    {
+        if (onRotating)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, 180), rotateSpeed);
+            rotateSpeed = rotateSpeed * Time.deltaTime;
+        }
+        else
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+}
