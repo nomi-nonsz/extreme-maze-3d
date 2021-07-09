3f96395e22a57c996176ce0e8382dd1182ba20b2
norman-andrians <norman25.na@gmail.com>
2021-07-09 15:39:49 +0700

    memperbarui scene credits, menambahkan about dev beserta pengenalan, akun sosmed, dan tombol lapor atau umpan balik

---
+++ script/credits/changeCreditsAndDev.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class changeCreditsAndDev : MonoBehaviour
+{
+    public GameObject[] creditsGroup;
+    public GameObject[] devGroup;
+
+    public Text textTitle;
+
+    public void selectCredits()
+    {
+        textTitle.text = "Credits";
+
+        for (int i = 0; i < creditsGroup.Length; i++)
+        {
+            creditsGroup[i].SetActive(true);
+        }
+
+        for (int i = 0; i < devGroup.Length; i++)
+        {
+            devGroup[i].SetActive(false);
+        }
+    }
+
+    public void selectDev()
+    {
+        textTitle.text = "About";
+
+        for (int i = 0; i < creditsGroup.Length; i++)
+        {
+            creditsGroup[i].SetActive(false);
+        }
+
+        for (int i = 0; i < devGroup.Length; i++)
+        {
+            devGroup[i].SetActive(true);
+        }
+    }
+}
---
+++ script/credits/gotoUrl.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gotoUrl : MonoBehaviour
+{
+    public void toTheUrl(string devUrl)
+    {
+        Application.OpenURL(devUrl);
+    }
+}
