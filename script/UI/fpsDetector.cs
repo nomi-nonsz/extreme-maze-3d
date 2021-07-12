63df22b72986f69eca6ba4419cb23f0304cd260f
norman-andrians <norman25.na@gmail.com>
2021-07-12 17:25:46 +0700

    version: 0.45, mengganti jangka waktu jalannya fps

--- script/UI/fpsDetector.cs
+++ script/UI/fpsDetector.cs
@@ -8,6 +8,10 @@ public class fpsDetector : MonoBehaviour
     public Text fpsText;
     public GameObject fpsObject;
 
+    public float fpsDelay = 1f;
+
+    private float fps;
+
     DebugSystem debug;
     LoadOptionInLevel options;
 
@@ -24,12 +28,22 @@ public class fpsDetector : MonoBehaviour
             Debug.Log(debug.gameDebug + debug.uiDebug + "FPS Displayed");
         else
             Debug.Log(debug.gameDebug + debug.uiDebug + "FPS Hidden");
+
+        StartCoroutine(RecalculateFps());
+    }
+
+    private IEnumerator RecalculateFps()
+    {
+        while (true)
+        {
+            fps = 1 / Time.deltaTime;
+            yield return new WaitForSeconds(fpsDelay);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        float fps = 1 / Time.deltaTime;
         fpsText.text = "FPS: " + Mathf.Round(fps);
     }
 }
